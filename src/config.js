f6e43b67b4c235e60c8c1360d75c6bf9dbc9c02c
Botdeveloperbd <sohrabminitipsx@gmail.com>
2025-08-17 10:48:26 +0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -24,10 +24,10 @@ const config = {
   
   // Production Environment
   production: {
-    API_BASE_URL: 'https://web-production-f200.up.railway.app',
-    SOCKET_URL: 'https://web-production-f200.up.railway.app',
-    MEDIA_BASE_URL: 'https://web-production-f200.up.railway.appp/media',
-    FRONTEND_URL: 'https://admin-8f9s.onrender.com'
+    API_BASE_URL: 'https://autojoin1-production.up.railway.app/',
+    SOCKET_URL: 'https://autojoin1-production.up.railway.app/',
+    MEDIA_BASE_URL: 'https://autojoin1-production.up.railway.app/media',
+    FRONTEND_URL: 'https://admin1-o77n.onrender.com'
   },
   
   // Staging Environment
@@ -40,10 +40,10 @@ const config = {
   
   // Render Environment (for Render deployment)
   render: {
-    API_BASE_URL: 'https://web-production-f200.up.railway.app',
-    SOCKET_URL: 'https://web-production-f200.up.railway.app',
-    MEDIA_BASE_URL: 'https://web-production-f200.up.railway.appp/media',
-    FRONTEND_URL: 'https://admin-8f9s.onrender.com'
+    API_BASE_URL: 'https://autojoin1-production.up.railway.app/',
+    SOCKET_URL: 'https://autojoin1-production.up.railway.app/',
+    MEDIA_BASE_URL: 'https://autojoin1-production.up.railway.app/media',
+    FRONTEND_URL: 'https://admin1-o77n.onrender.com'
   }
 };
 
@@ -134,5 +134,6 @@ const defaultExport = {
 };
 
 export default defaultExport; 
+
 
 
